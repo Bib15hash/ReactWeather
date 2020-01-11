c57912949af0fedb4fd1b3a3befa415c2a4542c9
Bib15hash <bibhashsarkar15042001@gmail.com>
2020-01-11 14:28:37 +0530

    Fix error modal

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -58,7 +58,7 @@ componentWillReceiveProps: function(newProps){
     function renderMessage (){
 
       if (isLoading){
-        return <h3 classname="text-center">Fetching Weather...</h3>;
+        return <h3 className="text-center">Fetching Weather...</h3>;
       }
       else if (temp && location){
         return <WeatherMessage temp={temp} location = {location}/>;
